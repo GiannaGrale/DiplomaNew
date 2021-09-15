52c2511303712c1d872a59730f94090c244f471c
brebe <b.rebert@mail.ru>
2021-09-15 14:17:54 +0300

    CrossBrowser refactor

--- src/test/java/utils/CrossBrowser.java
+++ src/test/java/utils/CrossBrowser.java
@@ -3,6 +3,7 @@ package utils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 
@@ -13,16 +14,16 @@ public class CrossBrowser {
     public static WebDriver selectDriver (String browser) {
         switch (browser) {
             case "Chrome":
-                WebDriverManager.chromedriver().setup ();
+                WebDriverManager.chromedriver().setup();
                 driver = new ChromeDriver();
                 break;
             case "Firefox":
-                WebDriverManager.firefoxdriver().setup ();
+                WebDriverManager.firefoxdriver().setup();
                 driver = new FirefoxDriver();
                 break;
             case "Edge":
-                WebDriverManager.edgedriver ().setup ();
-                driver = new FirefoxDriver();
+                WebDriverManager.edgedriver().setup();
+                driver = new EdgeDriver();
                 break;
             default:
                 throw new AssertionError("This browser is not supported");
