815ac9ca8fa044558b362c77c9e3bee3d37ddc8d
Anna <giannagrale@gmail.com>
2021-09-15 11:04:57 +0300

    BaseCrossBrowser.java refactor to test class

--- src/test/java/baseEntities/BaseCrossBrowser.java
+++ src/test/java/baseEntities/BaseCrossBrowser.java
@@ -1,18 +1,11 @@
 package baseEntities;
 
 import core.ReadProperties;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.opera.OperaDriver;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Parameters;
+
 
 public class BaseCrossBrowser {
     public WebDriver driver;
@@ -24,19 +17,4 @@ public class BaseCrossBrowser {
         driver.quit();
     }
 
-    @BeforeMethod
-    @Parameters({"BrowserType"})
-    public void setUpBrowser(String browserType) {
-            properties = ReadProperties.getInstance();
-        if (browserType.equalsIgnoreCase("Opera")) {
-            WebDriverManager.operadriver().setup();
-            driver = new OperaDriver();
-        } else if (browserType.equalsIgnoreCase("Chrome")) {
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
-        } else if (browserType.equalsIgnoreCase("Firefox")) {
-            WebDriverManager.firefoxdriver().setup();
-            driver = new FirefoxDriver();
-        }
-    }
 }
--- src/test/java/tests/CrossBrowserTest.java
+++ src/test/java/tests/CrossBrowserTest.java
@@ -1,6 +1,11 @@
 package tests;
 
 import baseEntities.BaseCrossBrowser;
+import core.ReadProperties;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.Assert;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
@@ -11,8 +16,20 @@ import utils.Listener;
 @Listeners({Listener.class})
 public class CrossBrowserTest extends BaseCrossBrowser {
     @Test
-    public void crossBrowserTest() {
+    public void crossBrowserTest(String browserType) {
         logger.info("crossBrowserTest is started");
+        properties = ReadProperties.getInstance();
+        if (browserType.equalsIgnoreCase("Edge")) {
+            WebDriverManager.edgedriver().setup();
+            driver = new InternetExplorerDriver();
+        } else if (browserType.equalsIgnoreCase("Chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else if (browserType.equalsIgnoreCase("Firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        }
+
         ProjectStep projectStep = new LoginStep(driver)
                 .correctLogin(properties.getLogin(), properties.getPassword());
         Assert.assertEquals(projectStep.getDashboardPage().getDashboardButtonText().trim(), "DASHBOARD");
