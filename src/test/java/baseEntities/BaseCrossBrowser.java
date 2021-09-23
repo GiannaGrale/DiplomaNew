231a463594776ea5a00bcf0323ef4b51f2812711
brebe <b.rebert@mail.ru>
2021-09-23 17:06:40 +0300

    BaseCrossBrowser update

--- src/test/java/baseEntities/BaseCrossBrowser.java
+++ src/test/java/baseEntities/BaseCrossBrowser.java
@@ -5,6 +5,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import utils.CrossBrowser;
@@ -21,7 +22,7 @@ public class BaseCrossBrowser {
         driver.quit();
     }
 
-    @BeforeTest
+    @BeforeMethod
     @Parameters({"BrowserType"})
     public void setUp(String browserType) {
         properties = ReadProperties.getInstance();
